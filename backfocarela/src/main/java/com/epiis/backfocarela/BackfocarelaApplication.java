606455bf47345d2f61fd2bf03867745aa634bfaf
ROGER <232175@unamba.edu.pe>
2026-06-14 17:48:19 -0500

    subiendo el backend

---
+++ backfocarela/src/main/java/com/epiis/backfocarela/BackfocarelaApplication.java
@@ -0,0 +1,13 @@
+package com.epiis.backfocarela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackfocarelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackfocarelaApplication.class, args);
+	}
+
+}
---
+++ backfocarela/src/test/java/com/epiis/backfocarela/BackfocarelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epiis.backfocarela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackfocarelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
